4549d5ddbff0c4b9770ff6b9efac5c8a9a4913a4
oliver-newman <onewman1@swarthmore.edu>
2016-08-19 14:28:49 -0400

    Added UserProfile model, and deconstruct methods

--- activities/admin.py
+++ activities/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.models import User
-from .models import Activity, Shoe, Bike
+from .models import UserProfile, Shoe, Bike, Activity
 
-admin.site.register(Activity)
+admin.site.register(UserProfile)
 admin.site.register(Shoe)
 admin.site.register(Bike)
+admin.site.register(Activity)
